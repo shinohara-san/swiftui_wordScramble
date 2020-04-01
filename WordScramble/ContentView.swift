6781d95bb7a1c5ca7ab322b7415dfefc10cc51d4
shinohara-san <someiyoshino0809@gmail.com>
2020-04-01 12:00:51 +0900

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Yuki Shinohara on 2020/04/01.
+//  Copyright © 2020 Yuki Shinohara. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
